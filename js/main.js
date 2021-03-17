e3629da8a4ae9e8d9c1fe92453003368b2deb712
Stuart Klingman <stuartk@avatek.com>
2021-03-16 22:52:51 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+/*----- constants -----*/
+const guessRemaining;
+const nextMonsterTile;
+cont howToPlayURL = “html/howtoplay.html”; /* Help doc must include link back to game */
+
+const playTotallyOriginalThemeSong = false; // default, bool
+const gameTitleMessage = “Welcome to the Monster Portal!” /* Game messages such as correct letters, Game wins & losses, and impending seismic events are displayed here. */
+const gameTitle = “MonsterPortal™”; // game title. Duh.
+
+/*----- Scoreboard constants -----*/
+const badGuesses;
+const correctGuesses;
+const totalGuessesMade;
+const gameWins;
+
+/*------ sound declarations, which are blatantly ignoring the clearly articulated
+         best practices of seperation of declaration vs initialization.   ------*/
+const audioCheer = new Audio('sounds/cheer.mp3');
+const audioTaDa = new Audio('sounds/tada.mp3');
+const audioMonsterPortalThemeSong = new Audio('sounds/monsterportal-themesong.mp3');
+audioMonsterPortalThemeSong.volume = .4;
+const audioCowbell = new Audio('sounds/cowbell.mp3');
+const audioQuit = new Audio('sounds/quit.mp3');
+const audioSoSad = new Audio('sounds/toobad.mp3');
+const audioMonsterRoar = Audio('sounds/mp-roar.mp3')
+
+
+
+/*----- app's state (variables) -----*/
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+/*----- event listeners -----*/
+// document.getElementById('demo-button').addEventListener('click', renderGame;
+
+
+
+/*----- functions -----*/
+
+function renderGame() {
+
+}
+
+//   setTimeout(() => {initializeGame(); }, 3000);
+//   setTimeout(() => {renderGame(); }, 4000);
+  
+
+/*----- Game Functions -----*/
+function initializeGame() {
+   console.log("\nGame Initialized.");
+};
+
+function renderGame() {
+  console.log("\nGame Rendered.");
+};
+
+
+/*----- Classes -----*/
+class letter {
+
+};
+
+class word {
+
+};
+
+class secretPasswords {
+
+};
+
+class keytopObject {
+
+};
+
+class keyboardObjectArray {
+
+};
+
+class activeSecretPassword {
+
+};
+
+class demoScript {
+
+};
+
+/*----- Start of Game -----*/
+
+
+
+
