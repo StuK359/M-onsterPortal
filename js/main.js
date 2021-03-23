fa365fa6c44d73be203e7c6ace18fe4ff5fb3d56
Stuart Klingman <stuartk@avatek.com>
2021-03-23 14:26:54 -0700

    Final Push #2

--- js/main.js
+++ js/main.js
@@ -25,12 +25,9 @@ const listSecretPasswords = [
   "CEREBELLUM"   /* 21 */
   ]; 
 
-
 /*------ sound declarations -----*/
 const audioCheer = new Audio('sounds/cheer.mp3');
 const audioTaDa = new Audio('sounds/tada.mp3');
-// const audioMonsterPortalThemeSong = new Audio('sounds/monsterportal-themesong.mp3');
-// audioMonsterPortalThemeSong.volume = .4;
 const audioCowbell = new Audio('sounds/cowbell.mp3');
 const audioQuit = new Audio('sounds/quit.mp3');
 const audioSoSad = new Audio('sounds/toobad.mp3');
@@ -62,29 +59,10 @@ var tile7 = "imgs/mp7.jpg";
 
 var nextMonsterTile = tile1;
 
-//Hiding the image using regular JavaScript.
-mtImg1 = document.getElementById('mt-img-1');
-mtImg1.style.display = "none";
-mtImg2 = document.getElementById('mt-img-2');
-mtImg2.style.display = "none";
-mtImg3 = document.getElementById('mt-img-3');
-mtImg3.style.display = "none";
-mtImg4 = document.getElementById('mt-img-4');
-mtImg4.style.display = "none";
-mtImg5 = document.getElementById('mt-img-5');
-mtImg5.style.display = "none";
-mtImg6 = document.getElementById('mt-img-6');
-mtImg6.style.display = "none";
-mtImg7 = document.getElementById('mt-img-7');
-mtImg7.style.display = "none";
-
-
 /*----- event listeners -----*/
-// document.getElementById('demo-button').addEventListener('click', renderGame);
 
-// Every keytop button needs an event handler that determines whether a click was 
-// a correct guess or an incorrect guess. Something like isGuessCorrect(guess).
-// Temorary test until full renderGame() is completed.
+// Every keytop button needs an event handler to process guess.
+// This code should be improved with https://javascript.info/event-delegation .
 document.getElementById('A').addEventListener('click', isGuessCorrect);
 document.getElementById('B').addEventListener('click', isGuessCorrect);
 document.getElementById('C').addEventListener('click', isGuessCorrect);
@@ -112,7 +90,7 @@ document.getElementById('X').addEventListener('click', isGuessCorrect);
 document.getElementById('Y').addEventListener('click', isGuessCorrect);
 document.getElementById('Z').addEventListener('click', isGuessCorrect);
 
-/* Attach initializeGame() to "Reset Game" button. */
+/* Attach initializeGame() to "Start" & "Reset" buttons. */
 document.getElementById('reset-game-btn').addEventListener('click', initGame);
 document.getElementById('start-game-btn').addEventListener('click', initGame);
 
@@ -121,6 +99,7 @@ gamesWon = 0;
 gamesLost = 0;
 
 function initGame() {
+  // Starting new game.
   gameOver = false;
 
 /* Hide monster: */
@@ -140,147 +119,45 @@ function initGame() {
   mtImg7.style.display = "none";
 
 /* Initialize Scoreboard: */
-badGuessesLeft = 7;
-document.getElementById("bad-guesses-left").innerHTML = badGuessesLeft;
-document.getElementById("games-won").innerHTML = gamesWon;
-document.getElementById("games-lost").innerHTML = gamesLost;
+  badGuessesLeft = 7;
+  document.getElementById("bad-guesses-left").innerHTML = badGuessesLeft;
+  document.getElementById("games-won").innerHTML = gamesWon;
+  document.getElementById("games-lost").innerHTML = gamesLost;
 
 /* Choose Secret Password */
-
-chooseSecretPassword();
+  chooseSecretPassword();
 
 /* Show the password length in scoreboard */
-spwdLength = SecretPassword.length;
-document.getElementById('spwd-length').innerHTML = spwdLength;
+  spwdLength = SecretPassword.length;
+  document.getElementById('spwd-length').innerHTML = spwdLength;
 
 // Clear the Secret Password field, then mask out the Secret Password letters:
-clearSecretPasswordFields();
+  clearSecretPasswordFields();
 
-for (var i=0; i < spwdLength;i++ ) {
-  document.getElementById(`sp-l${i}`).style.borderColor = "yellow";
-  document.getElementById(`sp-l${i}`).innerHTML = "*";
-};
+  for (var i=0; i < spwdLength;i++ ) {
+    document.getElementById(`sp-l${i}`).style.borderColor = "yellow";
+    document.getElementById(`sp-l${i}`).innerHTML = "*";
+  };
   guess = "";
-
   resetKeytops();
-}
-
-
-/*----- Classes -----*/
-// None used.
-
-/*----- Start of Game -----*/
-initGame();
-
-/* Is guess good or bad? */
-
-/* Process Bad Guess */
-function processBadGuess(guess) {
-  audioSoSad.play();
-  showNextMonsterTile(nextMonsterTile);
-  document.getElementById('bad-guesses-left').innerHTML = badGuessesLeft.toString();
-  document.getElementById('msg-game-banner').innerHTML = "WRONG!!! The monster is getting closer!!!";
-  if (badGuessesLeft) {
-    return; 
-  } else {
-    gameIsLost();
-    return;
-  }
-};
-
-/* Process Good Guess */
-function processGoodGuess(guess) {
-  audioTaDa.play();
-  // debugger
-  for (var i = 0; i < SecretPassword.length; i++) {
-    if (SecretPasswordLetterArray[i] === guess) {
-      document.getElementById(`sp-l${i}`).innerHTML = guess;
-      secretLettersRemaining = secretLettersRemaining-1;
-      console.log("secretLettersRemaining: ", secretLettersRemaining);      
-     }; 
-  }
-  document.getElementById('msg-game-banner').innerHTML = "EXCELLENT!!! One letter closer to saving the world!";
-  // ToDo: Remove letter from secret password letter array
-// console.log(SecretPasswordLetterArray);
-  if (!secretLettersRemaining) {
-    return gameIsWon();
-  } else {
-    return;
-  } 
-};
-
-function showNextMonsterTile(nextMonsterTile) {
-  showMonsterTile(nextMonsterTile);
-};
-
-function showMonsterTile(nextMonsterTile) {
-   switch(badGuessesLeft) {
-    // setTile1Visible
-    case 7:
-      mtImg1.style.display = "list-item";
-      badGuessesLeft = 6;
-    break;
-    
-    case 6:
-      mtImg2.style.display = "list-item";
-      badGuessesLeft = 5;
-      break;
-    
-    case 5:
-      mtImg3.style.display = "list-item";
-      badGuessesLeft = 4;
-      break;
-
-    case 4:
-      mtImg4.style.display = "list-item";
-      badGuessesLeft = 3;
-      break;
-
-    case 3:
-      mtImg5.style.display = "list-item";
-      badGuessesLeft = 2;
-      break;
-
-    case 2:
-      mtImg6.style.display = "list-item";
-      badGuessesLeft = 1;
-      break;
-    
-    case 1:
-      mtImg7.style.display = "list-item";
-      badGuessesLeft = 0;
-      break;
-   }
-};
-
-
-function gameIsWon () {
-  audioCheer.play();
-  gamesWon++;
-  gameOver = true;
-  document.getElementById('games-won').innerHTML = gamesWon.toString();
-  document.getElementById('msg-game-banner').innerHTML = "You Won! Play Again? Press 'Start Game'!";
-};
-
-function gameIsLost() {
-   // Set msgGameBanner to, "You lost!"
-   // After a short pause, set msgGameBanner to "Play Again?"
-   gamesLost++;
-   gameOver = true;
-   document.getElementById('games-lost').innerHTML = gamesLost.toString();
-   document.getElementById('msg-game-banner').innerHTML = "You Lost! Play Again? Press 'Start Game'!";
 };
 
 /* Random Secret Password picker: */
 function chooseSecretPassword() {
-   var idxSecretPassword = Math.floor(Math.random() * 20) + 1;
-
-   SecretPassword = listSecretPasswords[idxSecretPassword];
-   secretLettersRemaining = SecretPassword.length;
-   SecretPasswordLetterArray = SecretPassword.split('');
-   
+  var idxSecretPassword = Math.floor(Math.random() * 20) + 1;
+  SecretPassword = listSecretPasswords[idxSecretPassword];
+  secretLettersRemaining = SecretPassword.length;
+  SecretPasswordLetterArray = SecretPassword.split('');
 };
 
+function clearSecretPasswordFields() {
+  for (var i=0; i < 11; i++ ) {
+    document.getElementById(`sp-l${i}`).style.borderColor = "rgb(30,113,65)";
+    document.getElementById(`sp-l${i}`).innerHTML = " ";
+  };
+};
+
+/* Is guess good or bad? */
 function isGuessCorrect(guess) {
   var currentKeytopColor = this.style.backgroundColor;
   if (gameOver === true) return; // Ignore keyboard clicks and bail immediately if the game is over.
@@ -292,7 +169,7 @@ function isGuessCorrect(guess) {
     return;
   };
   
-  // Snag the keytop that player clicked for further processing 
+  // Snag the keytop that player clicked for further processing. 
   guess = this.id; 
   
   // luckyGuess will be >= zero if it's a good guess.
@@ -307,11 +184,90 @@ function isGuessCorrect(guess) {
   return luckyGuess;
 };
 
-function clearSecretPasswordFields() {
-  for (var i=0; i < 11; i++ ) {
-    document.getElementById(`sp-l${i}`).style.borderColor = "rgb(30,113,65)";
-    document.getElementById(`sp-l${i}`).innerHTML = " ";
-  };
+/* Process Good Guess */
+function processGoodGuess(guess) {
+  for (var i = 0; i < SecretPassword.length; i++) {
+    if (SecretPasswordLetterArray[i] === guess) {
+      document.getElementById(`sp-l${i}`).innerHTML = guess;
+      secretLettersRemaining = secretLettersRemaining-1;
+    }; 
+  }
+  document.getElementById('msg-game-banner').innerHTML = "EXCELLENT!!! One letter closer to saving the world!";
+  // No more secret letters left, they won!
+  if (!secretLettersRemaining) {
+    return gameIsWon();
+  } else {
+    return;
+  } 
+};
+
+/* Process Bad Guess */
+function processBadGuess(guess) {
+  showNextMonsterTile(nextMonsterTile);
+  document.getElementById('bad-guesses-left').innerHTML = badGuessesLeft.toString();
+  document.getElementById('msg-game-banner').innerHTML = "WRONG!!! The monster is getting closer!!!";
+  if (badGuessesLeft) {
+    return; 
+  } else {
+    gameIsLost();
+    return;
+  }
+};
+
+function showNextMonsterTile(nextMonsterTile) {
+  showMonsterTile(nextMonsterTile);
+};
+
+function showMonsterTile(nextMonsterTile) {
+   switch(badGuessesLeft) {
+    // setTile1Visible
+    case 7:
+      mtImg1.style.display = "list-item";
+      badGuessesLeft = 6;
+    break;
+    case 6:
+      mtImg2.style.display = "list-item";
+      badGuessesLeft = 5;
+      break;
+    case 5:
+      mtImg3.style.display = "list-item";
+      badGuessesLeft = 4;
+      break;
+    case 4:
+      mtImg4.style.display = "list-item";
+      badGuessesLeft = 3;
+      break;
+    case 3:
+      mtImg5.style.display = "list-item";
+      badGuessesLeft = 2;
+      break;
+    case 2:
+      mtImg6.style.display = "list-item";
+      badGuessesLeft = 1;
+      break;
+    case 1:
+      mtImg7.style.display = "list-item";
+      badGuessesLeft = 0;
+      break;
+   }
+};
+
+function gameIsWon () {
+  audioCheer.play();
+  gamesWon++;
+  gameOver = true;
+  document.getElementById('games-won').innerHTML = gamesWon.toString();
+  document.getElementById('msg-game-banner').innerHTML = "You Won! Play Again? Press 'Start Game'!";
+};
+
+function gameIsLost() {
+   // Set msgGameBanner to, "You lost!"
+   // After a short pause, set msgGameBanner to "Play Again?"
+   gamesLost++;
+   gameOver = true;
+   audioSoSad.play();
+   document.getElementById('games-lost').innerHTML = gamesLost.toString();
+   document.getElementById('msg-game-banner').innerHTML = "You Lost! Play Again? Press 'Start Game'!";
 };
 
 function resetKeytops() {
@@ -343,3 +299,25 @@ function resetKeytops() {
   document.getElementById('Y').style.backgroundColor = "grey";
   document.getElementById('Z').style.backgroundColor = "grey";  
 };
+
+/*----- Start of Game -----*/
+// Hiding the Monster image after load.
+mtImg1 = document.getElementById('mt-img-1');
+mtImg1.style.display = "none";
+mtImg2 = document.getElementById('mt-img-2');
+mtImg2.style.display = "none";
+mtImg3 = document.getElementById('mt-img-3');
+mtImg3.style.display = "none";
+mtImg4 = document.getElementById('mt-img-4');
+mtImg4.style.display = "none";
+mtImg5 = document.getElementById('mt-img-5');
+mtImg5.style.display = "none";
+mtImg6 = document.getElementById('mt-img-6');
+mtImg6.style.display = "none";
+mtImg7 = document.getElementById('mt-img-7');
+mtImg7.style.display = "none";
+
+// Do Everything, because Stu couldn't stop touching his DOM. 
+// (now he's gone blind <sad_face>)  
+initGame();
+
